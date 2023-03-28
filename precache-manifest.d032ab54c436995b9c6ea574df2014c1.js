f7404ef3983c8faa09d76bf0bd7e2976af72911d
rain <1063044718@qq.com>
2023-03-28 13:37:23 +0800

    deploy

---
+++ precache-manifest.d032ab54c436995b9c6ea574df2014c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa0ce1be5f834d77004",
+    "url": "/money-webSite/css/app.ed8b515f.css"
+  },
+  {
+    "revision": "87afbc73d88607bc9e15e1dc6ce09ba2",
+    "url": "/money-webSite/index.html"
+  },
+  {
+    "revision": "0fa0ce1be5f834d77004",
+    "url": "/money-webSite/js/app.a1f5647e.js"
+  },
+  {
+    "revision": "e18ad45c78cef3531723",
+    "url": "/money-webSite/js/chunk-vendors.8357ffec.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/money-webSite/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-webSite/robots.txt"
+  }
+]);
